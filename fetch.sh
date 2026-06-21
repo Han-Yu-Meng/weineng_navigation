b565c38b41d13fb76c65a24d3554d256794c1b29
huigg <wangjinghui@sjtu.edu.cn>
2026-06-21 13:24:41 +0800

    feat: 更新底层FineNav-Engine库
    更新原因：
    1. 修复mppi threshold_to_consider未实现的问题（对导航效果影响巨大）
    2. localizer支持force_2d_mode

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,7 @@
 # 克隆FineNav-Engine
 git clone -b dev --single-branch https://github.com/huigg296/FineNav-Engine.git
 cd ./FineNav-Engine/
-git checkout 3956343
+git checkout abf75d0
 
 # 删除FineNav-Engine原有的demo
 rm -rf ./finenav_demo/
