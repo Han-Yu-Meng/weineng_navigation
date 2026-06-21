c9638ec03824fbe25d025cff90d9e80afbe30a4e
huigg <wangjinghui@sjtu.edu.cn>
2026-06-21 12:48:54 +0800

    feat: 更新底层库FineNav-Engine版本

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,7 @@
 # 克隆FineNav-Engine
 git clone -b dev --single-branch https://github.com/huigg296/FineNav-Engine.git
 cd ./FineNav-Engine/
-git checkout 3cfab4a
+git checkout 3956343
 
 # 删除FineNav-Engine原有的demo
 rm -rf ./finenav_demo/
